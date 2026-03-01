48719bf169127b9e3fde58d4297eeab93abd6d3d
Ashwithaaverineni <ashwithaaverineni7891@gmail.com>
2026-03-01 21:26:43 +0530

    recommendation logic

---
+++ recommend.py
@@ -0,0 +1,20 @@
+import pickle
+
+# Load model
+movies, similarity = pickle.load(open('../model/recommender.pkl', 'rb'))
+
+def recommend(movie_name):
+    movie_index = movies[movies['title'] == movie_name].index[0]
+    distances = similarity[movie_index]
+
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommendations = []
+    for i in movie_list:
+        recommendations.append(movies.iloc[i[0]].title)
+
+    return recommendations
+
+
+# Test
+print(recommend("Inception"))
